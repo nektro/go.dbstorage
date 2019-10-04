0fd5f7b37dd94d6b342ed939edd31335e0efaabc
Meghan Denny <nektromc@gmail.com>
2019-10-04 16:13:40 -0400

    Database - remove hardcoded functions, add query builder

--- dbstorage.go
+++ dbstorage.go
@@ -11,17 +11,21 @@ type Database interface {
 	CreateTable(name string, pk []string, columns [][]string)
 	CreateTableStruct(name string, v interface{})
 	DoesTableExist(table string) bool
-	Query(modify bool, q string) *sql.Rows
+	Build() QueryBuilder
 	QueryColumnList(table string) []string
 	QueryNextID(table string) int64
 	QueryPrepared(modify bool, q string, args ...interface{}) *sql.Rows
-	QueryDoSelectAll(table string) *sql.Rows
-	QueryDoSelect(table string, where string, search string) *sql.Rows
-	QueryDoSelectAnd(table string, where string, search string, where2 string, search2 string) *sql.Rows
-	QueryDoUpdate(table string, col string, value string, where string, search string)
-	QueryDoSelectAllOrder(table string, order string) *sql.Rows
-	QuerySelectFunc(table string, sfunc string, col string, haystack string, needle string) *sql.Rows
-	QueryDelete(table string, col string, search string)
+}
+
+type QueryBuilder interface {
+	Se(cols string) QueryBuilder
+	Fr(tabls string) QueryBuilder
+	Wh(col string, value string) QueryBuilder
+	An(col string, value string) QueryBuilder
+	Or(col string, order string) QueryBuilder
+	Up(table string, col string, value string) QueryBuilder
+	//
+	Exe() *sql.Rows
 }
 
 func QueryHasRows(query *sql.Rows) bool {

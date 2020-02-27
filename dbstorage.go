d9c9defa68409668802b1c50d3b8a12bf6f7e923
Meghan Denny <nektromc@gmail.com>
2020-02-27 10:18:50 -0500

    add function comments

--- dbstorage.go
+++ dbstorage.go
@@ -8,6 +8,7 @@ import (
 )
 
 var (
+	// StatementDebug - Enable this flag to print all executed SQL statements.
 	StatementDebug bool
 )
 
@@ -16,15 +17,18 @@ func init() {
 }
 
 var (
+	// InsertsLock - use this so that Database.QueryNextID and DataBase.Build.Ins happen in an atomic fashion.
 	InsertsLock = new(sync.Mutex)
 )
 
+// QueryHasRows checks if a Rows response contains any values, and then closes the query.
 func QueryHasRows(query *sql.Rows) bool {
 	b := query.Next()
 	query.Close()
 	return b
 }
 
+// ScanAll scans all possible values of a QueryBuilder into an array based on template Scannable.
 func ScanAll(qb QueryBuilder, s Scannable) []Scannable {
 	result := []Scannable{}
 	rows := qb.Exe()
@@ -35,6 +39,7 @@ func ScanAll(qb QueryBuilder, s Scannable) []Scannable {
 	return result
 }
 
+// ScanFirst scans the first value from the QueryBuilder, then closes the query.
 func ScanFirst(qb QueryBuilder, s Scannable) Scannable {
 	rows := qb.Exe()
 	defer rows.Close()

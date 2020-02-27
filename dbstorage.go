5f23521486f945503136da66433004d9f0b73bba
Meghan Denny <nektromc@gmail.com>
2020-02-27 10:14:40 -0500

    move interfaces to new file

--- dbstorage.go
+++ dbstorage.go
@@ -15,48 +15,14 @@ func init() {
 	pflag.BoolVar(&StatementDebug, "dbstorage-debug-sql", false, "Enable this flag to print all executed SQL statements.")
 }
 
-type Database interface {
-	Ping() error
-	Close() error
-	DB() *sql.DB
-	CreateTable(name string, pk []string, columns [][]string)
-	CreateTableStruct(name string, v interface{})
-	DoesTableExist(table string) bool
-	Build() QueryBuilder
-	QueryColumnList(table string) []string
-	QueryNextID(table string) int64
-	QueryPrepared(modify bool, q string, args ...interface{}) *sql.Rows
-}
-
 var (
 	InsertsLock = new(sync.Mutex)
 )
-
-type QueryBuilder interface {
-	Se(cols string) QueryBuilder
-	Fr(tabls string) QueryBuilder
-	WR(col string, op string, value string, raw bool) QueryBuilder
-	Wr(col string, op string, value string) QueryBuilder
-	Wh(col string, value string) QueryBuilder
-	Or(col string, order string) QueryBuilder
-	Lm(limit int64) QueryBuilder
-	Of(offset int64) QueryBuilder
-	//
-	Up(table string, col string, value string) QueryBuilder
-	//
-	Ins(table string, values ...interface{}) QueryBuilder
-	//
-	Exe() *sql.Rows
-}
 
 func QueryHasRows(query *sql.Rows) bool {
 	b := query.Next()
 	query.Close()
 	return b
-}
-
-type Scannable interface {
-	Scan(rows *sql.Rows) Scannable
 }
 
 func ScanAll(qb QueryBuilder, s Scannable) []Scannable {
---
+++ t.go
@@ -0,0 +1,37 @@
+package dbstorage
+
+import "database/sql"
+
+type Database interface {
+	Ping() error
+	Close() error
+	DB() *sql.DB
+	CreateTable(name string, pk []string, columns [][]string)
+	CreateTableStruct(name string, v interface{})
+	DoesTableExist(table string) bool
+	Build() QueryBuilder
+	QueryColumnList(table string) []string
+	QueryNextID(table string) int64
+	QueryPrepared(modify bool, q string, args ...interface{}) *sql.Rows
+}
+
+type QueryBuilder interface {
+	Se(cols string) QueryBuilder
+	Fr(tabls string) QueryBuilder
+	WR(col string, op string, value string, raw bool) QueryBuilder
+	Wr(col string, op string, value string) QueryBuilder
+	Wh(col string, value string) QueryBuilder
+	Or(col string, order string) QueryBuilder
+	Lm(limit int64) QueryBuilder
+	Of(offset int64) QueryBuilder
+	//
+	Up(table string, col string, value string) QueryBuilder
+	//
+	Ins(table string, values ...interface{}) QueryBuilder
+	//
+	Exe() *sql.Rows
+}
+
+type Scannable interface {
+	Scan(rows *sql.Rows) Scannable
+}
